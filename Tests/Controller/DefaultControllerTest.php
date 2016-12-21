2c502d71c90c58e6d8db51f2ad05af6630de155b
beyerz <bailz777@gmail.com>
2016-12-21 12:06:53 +0200

    fixed controller testing, moved away from annotations as they are not loaded properly when unit testing added twig so we can render templates for testing updated the bootstrap

--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -2,10 +2,13 @@
 
 namespace Beyerz\CheckBookIOBundle\Tests\Controller;
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Beyerz\CheckBookIOBundle\Controller\DefaultController;
 
 class DefaultControllerTest extends WebTestCase
 {
+
     public function testIndex()
     {
         $client = static::createClient();
--- Tests/Fixtures/App/app/AppKernel.php
+++ Tests/Fixtures/App/app/AppKernel.php
@@ -7,6 +7,7 @@ class AppKernel extends Kernel
     {
         return array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
             new \Beyerz\CheckBookIOBundle\CheckBookIOBundle(),
         );
     }
