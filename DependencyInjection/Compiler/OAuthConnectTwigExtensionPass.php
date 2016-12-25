c78efaade4b51068e00d3260fd1e0483109d1858
beyerz <bailz777@gmail.com>
2016-12-25 18:23:36 +0200

    removed calback url from configs added compiler pass to inject router into twig extension updated twig extension to generate connect url from router

---
+++ DependencyInjection/Compiler/OAuthConnectTwigExtensionPass.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bailz777
+ * Date: 25/12/2016
+ * Time: 16:18
+ */
+
+namespace Beyerz\CheckBookIOBundle\DependencyInjection\Compiler;
+
+
+use Beyerz\CheckBookIOBundle\Twig\Extensions\OauthConnectExtension;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class OAuthConnectTwigExtensionPass implements CompilerPassInterface
+{
+
+    /**
+     * We can only add the router now as it is not definitely available when the bundle loads
+     *
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $twigExtension = $container->findDefinition('beyerz.checkbookio.twig.oauth_connect');
+        $twigExtension->addArgument(new Reference($container->getAlias('router')));
+    }
+}
--- Twig/Extensions/OauthConnectExtension.php
+++ Twig/Extensions/OauthConnectExtension.php
@@ -9,33 +9,20 @@
 namespace Beyerz\CheckBookIOBundle\Twig\Extensions;
 
 
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 class OauthConnectExtension extends \Twig_Extension
 {
-    /**
-     * @var string
-     */
-    private $clientId;
 
     /**
-     * @var string
+     * @var Router
      */
-    private $redirectUri;
+    private $router;
 
-    /**
-     * @var string
-     */
-    private $sandbox;
-
-    /**
-     * @param $clientId
-     * @param $redirectUri
-     * @param $sandbox
-     */
-    public function __construct($clientId, $redirectUri, $sandbox)
+    public function __construct(Router $router)
     {
-        $this->clientId = $clientId;
-        $this->redirectUri = $redirectUri;
-        $this->sandbox = $sandbox;
+        $this->router = $router;
     }
 
     public function getFunctions()
@@ -51,16 +38,8 @@ class OauthConnectExtension extends \Twig_Extension
      */
     public function oauthConnect()
     {
-        $queryParams = [
-            'client_id' => $this->clientId,
-            'response_type' => 'code',
-            'scope' => 'check',
-            'redirect_uri' => $this->redirectUri
-        ];
-        $queryString = http_build_query($queryParams);
-        $href = sprintf('%s%s%s',$this->sandbox?'https://sandbox.checkbook.io':'https://checkbook.io','/oauth/authorize?',$queryString);
-        $href = 'http://www.testing.com:8001/app_dev.php/checkbookio/connect?redirect=true';
-        $html = '<a href="'.$href.'"><button>Checkbook Connect</button></a>';
+        $connectUrl = $this->router->generate('check_book_io_oauth_connect',['redirect'=>true],UrlGeneratorInterface::ABSOLUTE_URL);
+        $html = '<a href="'.$connectUrl.'"><button>Checkbook Connect</button></a>';
         return $html;
     }
 
